3d22fb10a5d4b7bcab7655b40b6ba2895044c679
Kevin Gong <kevingong1995@gmail.com>
2019-03-13 16:27:07 -0700

    included js and made changes to mobile navbar

---
+++ public/app.js
@@ -0,0 +1,18 @@
+const navSlider = () =>{
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+
+  burger.addEventListener('click', ()=>{
+    //Toggle Nav
+    nav.classList.toggle('nav-active');
+    //Burger Animation
+    burger.classList.toggle('toggle');
+  });
+
+  document.querySelector('.nav-links').addEventListener('click', ()=>{
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+  });
+}
+
+navSlider();
