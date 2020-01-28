b8943770660b97d0e900965d534cbcca2cefe66a
Kevin Gong <kevingong1995@gmail.com>
2020-01-27 22:03:03 -0800

    updates to portfolio

--- dist/app.js
+++ dist/app.js
@@ -2,19 +2,17 @@ const navSlider = () =>{
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
   const navLi = document.querySelector('.navbar ul');
-
+  //this is for the burger menu
   document.querySelector( "#nav-toggle" )
     .addEventListener( "click", function() {
       this.classList.toggle("active");
-      nav.classList.toggle('nav-active');
+      nav.classList.toggle("nav-active");
     });
-
- document.querySelector('.nav-links').addEventListener('click', (e)=>{
-    if(e.target.id){
+    $('.nav-links').click(function(e){
       nav.classList.toggle('nav-active');
-      document.querySelector( "#nav-toggle" ).classList.toggle("active");
-    }
-  });
+      $('#nav-toggle')[0].classList.toggle("active");
+    });
 }
+
 
 navSlider();
