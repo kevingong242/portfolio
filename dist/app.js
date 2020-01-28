fbcce65a6ef82f4cceb1544150d082ab43b8eb5d
Kevin Gong <kevingong1995@gmail.com>
2020-01-27 22:44:42 -0800

    adjusted navigation bar

--- dist/app.js
+++ dist/app.js
@@ -1,18 +1,24 @@
+const pages = ["homeNav", "experienceNav", "skillNav", "projectNav"];
 const navSlider = () =>{
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
   const navLi = document.querySelector('.navbar ul');
   //this is for the burger menu
-  document.querySelector( "#nav-toggle" )
-    .addEventListener( "click", function() {
-      this.classList.toggle("active");
-      nav.classList.toggle("nav-active");
-    });
-    $('.nav-links').click(function(e){
-      nav.classList.toggle('nav-active');
-      $('#nav-toggle')[0].classList.toggle("active");
-    });
+  $('#nav-toggle').click(function(){
+    this.classList.toggle("active");
+    nav.classList.toggle("nav-active");
+  });
+  $('.nav-links').click(function(e){
+    nav.classList.toggle('nav-active');
+    $('#nav-toggle')[0].classList.toggle("active");
+  });
 }
 
 
 navSlider();
+
+//Shows current page on navbar
+$('.nav-links li').click(function(e){
+  $('.current-page')[0].classList.remove("current-page");
+  e.target.parentNode.classList.add("current-page");
+});
