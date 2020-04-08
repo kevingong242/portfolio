b466880d4e0e08696f8ca8f95fbceae047334cf7
Kevin Gong <kevingong1995@gmail.com>
2020-04-07 21:59:47 -0700

    update to live portfolio

--- dist/app.js
+++ dist/app.js
@@ -1,24 +1,20 @@
-const pages = ["homeNav", "experienceNav", "skillNav", "projectNav"];
 const navSlider = () =>{
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
   const navLi = document.querySelector('.navbar ul');
-  //this is for the burger menu
-  $('#nav-toggle').click(function(){
-    this.classList.toggle("active");
-    nav.classList.toggle("nav-active");
-  });
-  $('.nav-links').click(function(e){
-    nav.classList.toggle('nav-active');
-    $('#nav-toggle')[0].classList.toggle("active");
+
+  document.querySelector( "#nav-toggle" )
+    .addEventListener( "click", function() {
+      this.classList.toggle("active");
+      nav.classList.toggle('nav-active');
+    });
+
+ document.querySelector('.nav-links').addEventListener('click', (e)=>{
+    if(e.target.id){
+      nav.classList.toggle('nav-active');
+      document.querySelector( "#nav-toggle" ).classList.toggle("active");
+    }
   });
 }
 
-
 navSlider();
-
-//Shows current page on navbar
-$('.nav-links li').click(function(e){
-  $('.current-page')[0].classList.remove("current-page");
-  e.target.parentNode.classList.add("current-page");
-});
